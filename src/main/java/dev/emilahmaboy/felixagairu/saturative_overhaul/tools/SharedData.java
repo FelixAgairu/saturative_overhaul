89a15953b2d92c2ed78ff048894d493f0784b400
Felix Agairu <FelixAgairu@outlook.com>
2025-04-11 20:21:37 +0800

    Use wxternal ConfigManager class 使用外部ConfigManager类

--- src/main/java/dev/emilahmaboy/felixagairu/saturative_overhaul/tools/SharedData.java
+++ src/main/java/dev/emilahmaboy/felixagairu/saturative_overhaul/tools/SharedData.java
@@ -1,11 +1,31 @@
 package dev.emilahmaboy.felixagairu.saturative_overhaul.tools;
 
 import com.google.gson.JsonObject;
+import dev.felixagairu.configmanager.ConfigManager;
 
 public class SharedData {
-    private static final JsonObject configs = ConfigManager.loadConfig();
 
-    public static final int foodLevel = Integer.parseInt(configs.get("foodLevel").getAsString());
+
+    private static final String defaultConfigs ="""
+        {
+        \t"foodLevel":400,
+        \t"defaultFoodLevel":280,
+        \t"nutritionModifier":5,
+        \t"nutritionRandLow":0.8,
+        \t"nutritionRandHigh":1.2,
+        \t"saturationRandLow":0.8,
+        \t"saturationRandHigh":1.2,
+        \t"thresholdStd":300,
+        \t"thresholdMin":25,
+        \t"thresholdLow":100,
+        \t"thresholdMid":250,
+        \t"thresholdHigh":350
+        }""";
+
+    private static final ConfigManager cM = new ConfigManager("saturative_overhaul.json", defaultConfigs);
+    private static final JsonObject configs = cM.loadConfig();
+
+    public static int foodLevel = Integer.parseInt(configs.get("foodLevel").getAsString());
     public static final int defaultFoodLevel = Integer.parseInt(configs.get("defaultFoodLevel").getAsString());
 
     public static final float nutritionModifier = Float.parseFloat(configs.get("nutritionModifier").getAsString());
